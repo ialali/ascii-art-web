04f8bdd3fc0b83004c53036c96bcdca7162e3c3e
Ibrahim Al-Ali <ibrahim.alali94@yahoo.ro>
2023-06-01 11:12:11 +0100

    is live

--- main.go
+++ main.go
@@ -1,3 +1,27 @@
 package main
 
-func main()
+import (
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	// Serve static files
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Define routes
+	http.HandleFunc("/", homeHandler)
+
+	// Start the server
+	http.ListenAndServe(":8000", nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, nil)
+}
