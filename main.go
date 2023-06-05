d8253c4093cbf7a6aa59b9de6f9f4db6ea6f7842
Ibrahim Al-Ali <ibrahim.alali94@yahoo.ro>
2023-06-05 17:43:19 +0100

    progress..

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"text/template"
 )
+
+type PageData struct {
+	asciiartcontainer string
+}
 
 func main() {
 	http.HandleFunc("/", handleIndex)
 	http.HandleFunc("/generate", handleGenerate)
-	http.HandleFunc("/fonts/", handleFonts)
+	// http.HandleFunc("/fonts/", handleFonts)
 
 	fmt.Println("Server started. Listening on http://localhost:8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
@@ -22,78 +25,60 @@ func main() {
 func handleIndex(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
+func renderTemplate(w http.ResponseWriter, templateFile string, data interface{}) {
+	tmpl := template.Must(template.ParseFiles(templateFile))
+	if err := tmpl.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
 func handleGenerate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-
-	// Read the request body
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
+	text := r.FormValue("text")
+	font := r.FormValue("font")
+	result := generateAsciiArt(text, font)
+	data := PageData{
+		asciiartcontainer: result,
 	}
-
-	// Parse the JSON data
-	var data struct {
-		Word string `json:"word"`
-		Font string `json:"font"`
-	}
-	err = json.Unmarshal(body, &data)
-	if err != nil {
-		http.Error(w, "Invalid request payload", http.StatusBadRequest)
-		return
-	}
-
-	// Generate the ASCII art
-	result, err := generateAsciiArt(data.Word, data.Font)
-	if err != nil {
-		http.Error(w, "Failed to generate ASCII art", http.StatusInternalServerError)
-		return
-	}
-
-	// Write the result as the response
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte(result))
+	renderTemplate(w, "index.html", data)
 }
 
 func handleFonts(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, r.URL.Path[1:])
 }
 
-func generateAsciiArt(word string, font string) (string, error) {
-	if word == "" {
-		return "", fmt.Errorf("Please enter a word")
-	}
-
-	filename := "fonts/standard.txt"
-	switch font {
-	case "shadow.txt":
-		filename = "fonts/shadow.txt"
-	case "thinkertoy.txt":
-		filename = "fonts/thinkertoy.txt"
-	}
-
-	content, err := os.ReadFile(filename)
+func generateAsciiArt(text, font string) string {
+	words := strings.Split(text, "\n")
+	rawBytes, err := os.ReadFile(fmt.Sprintf("fonts/%s.txt", font))
 	if err != nil {
-		return "", fmt.Errorf("Failed to read font file: %v", err)
+		log.Fatal(err)
 	}
 
-	lines := strings.Split(string(content), "\n")
+	lines := strings.Split(strings.ReplaceAll(string(rawBytes), "\r\n", "\n"), "\n")
 	var result strings.Builder
+	for i, word := range words {
+		if word == "" {
+			if i < len(words)-1 {
+				result.WriteString(word)
+			}
+			continue
+		}
 
-	for h := 1; h < 9; h++ {
-		for _, l := range word {
-			for lineIndex, line := range lines {
-				if lineIndex == (int(l)-32)*9+h {
-					result.WriteString(line)
+		for h := 1; h < 9; h++ {
+			for _, l := range word {
+				for lineIndex, line := range lines {
+					if lineIndex == (int(l)-32)*9+h {
+						result.WriteString(line)
+					}
 				}
 			}
+			result.WriteString("\n")
 		}
-		result.WriteString("\n")
-	}
 
-	return result.String(), nil
+		return result.String()
+	}
+	return result.String()
 }
