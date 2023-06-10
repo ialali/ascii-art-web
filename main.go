4d451445550cbe9c058eb5e1a442f0c7aab77299
Ibrahim Al-Ali <ibrahim.alali94@yahoo.ro>
2023-06-10 08:22:51 +0100

    revised and working

--- main.go
+++ main.go
@@ -2,29 +2,52 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"strings"
-	"text/template"
 )
 
 type PageData struct {
-	asciiartcontainer string
+	AsciiArt string
 }
 
 func main() {
-	http.HandleFunc("/", handleIndex)
-	http.HandleFunc("/generate", handleGenerate)
-	// http.HandleFunc("/fonts/", handleFonts)
-
-	fmt.Println("Server started. Listening on http://localhost:8000")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", handleMainPage)
+	http.HandleFunc("/ascii-art", handleAsciiArt)
+	fmt.Println("Server started. Listening on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func handleIndex(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
+func handleMainPage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 page not found ", http.StatusNotFound)
+		return
+	}
+	renderTemplate(w, "index.html", nil)
 }
+
+func handleAsciiArt(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	text := r.FormValue("text")
+	banner := r.FormValue("font")
+
+	result := generateAsciiArt(text, banner)
+
+	data := PageData{
+		AsciiArt: result,
+	}
+
+	renderTemplate(w, "index.html", data)
+}
+
 func renderTemplate(w http.ResponseWriter, templateFile string, data interface{}) {
 	tmpl := template.Must(template.ParseFiles(templateFile))
 	if err := tmpl.Execute(w, data); err != nil {
@@ -32,38 +55,19 @@ func renderTemplate(w http.ResponseWriter, templateFile string, data interface{}
 	}
 }
 
-func handleGenerate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	text := r.FormValue("text")
-	font := r.FormValue("font")
-	result := generateAsciiArt(text, font)
-	data := PageData{
-		asciiartcontainer: result,
-	}
-	renderTemplate(w, "index.html", data)
-}
-
-func handleFonts(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, r.URL.Path[1:])
-}
-
-func generateAsciiArt(text, font string) string {
-	words := strings.Split(text, "\n")
-	rawBytes, err := os.ReadFile(fmt.Sprintf("fonts/%s.txt", font))
+func generateAsciiArt(text, banner string) string {
+	words := strings.Split(text, `\n`)
+	rawBytes, err := os.ReadFile(fmt.Sprintf("fonts/%s.txt", banner))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	lines := strings.Split(strings.ReplaceAll(string(rawBytes), "\r\n", "\n"), "\n")
 	var result strings.Builder
-	for i, word := range words {
+
+	for _, word := range words {
 		if word == "" {
-			if i < len(words)-1 {
-				result.WriteString(word)
-			}
+			result.WriteString("\n")
 			continue
 		}
 
@@ -77,8 +81,7 @@ func generateAsciiArt(text, font string) string {
 			}
 			result.WriteString("\n")
 		}
-
-		return result.String()
 	}
+
 	return result.String()
 }
